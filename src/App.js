5afba447b07c96399d608d9e7f4d8ece945dd570
adilj13 <adil.j13@gmail.com>
2022-09-01 11:47:51 +0500

    init repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          PUCIT Help Desk
+        </p>
+        <p
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coming Soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
