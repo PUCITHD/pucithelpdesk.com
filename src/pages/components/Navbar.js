f023de504d606b6a982c69cf85ce6f3cbde8d32f
adilj13 <adil.j13@gmail.com>
2022-09-03 10:16:12 +0500

    fixed navbar

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props) {
             <nav className="bg-slate-100 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
                     <Link to="/" className="flex items-center">
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Adil Aziz</span>
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PUCIT Help Desk</span>
                     </Link>
                     <div className="flex md:order-2">
                         {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
